6e1b2c85a19470e981544e18d1cdeca29c56c131
Markus Palme <markuspalme@hotmail.com>
2018-02-04 19:52:24 +0100

    Updated readme.

---
+++ HmacAuthentication/HmacDelegatingHandler.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HmacAuthentication
+{
+    public class HmacDelegatingHandler : DelegatingHandler
+    {
+        #region Private members
+
+        private string appId;
+        private string apiKey;
+
+        #endregion
+
+        public HmacDelegatingHandler(string appId, string apiKey)
+        {
+            this.appId = appId;
+            this.apiKey = apiKey;
+
+            InnerHandler = new HttpClientHandler();
+        }
+
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            HttpResponseMessage response = null;
+
+            var requestContentBase64String = string.Empty;
+            var requestUri = WebUtility.UrlEncode(request.RequestUri.AbsoluteUri).ToLowerInvariant();
+            var requestHttpMethod = request.Method.Method;
+
+            // Calculate UNIX time
+            var epochStart = new DateTime(1970, 01, 01, 0, 0, 0, 0, DateTimeKind.Utc);
+            var timeSpan = DateTime.UtcNow - epochStart;
+            var requestTimeStamp = Convert.ToUInt64(timeSpan.TotalSeconds).ToString();
+
+            // Create random nonce for each request
+            var nonce = Guid.NewGuid().ToString("N");
+
+            // Checking if the request contains body, usually will be null with HTTP GET and DELETE
+            if (request.Content != null)
+            {
+                var content = await request.Content.ReadAsByteArrayAsync();
+                var md5 = MD5.Create();
+                // Hashing the request body, any change in request body will result in different hash, we'll incure message integrity
+                var requestContentHash = md5.ComputeHash(content);
+                requestContentBase64String = Convert.ToBase64String(requestContentHash);
+            }
+
+            // Creating the raw signature string
+            var signatureRawData = string.Format("{0}{1}{2}{3}{4}{5}", appId, requestHttpMethod, requestUri, requestTimeStamp, nonce, requestContentBase64String);
+
+            var secretKeyByteArray = Convert.FromBase64String(apiKey);
+            var signature = Encoding.UTF8.GetBytes(signatureRawData);
+
+            using (var hmac = new HMACSHA256(secretKeyByteArray))
+            {
+                var signatureBytes = hmac.ComputeHash(signature);
+                var requestSignatureBase64String = Convert.ToBase64String(signatureBytes);
+
+                // Setting the values in the Authorization header using custom scheme (Hmac)
+                request.Headers.Authorization = new AuthenticationHeaderValue("Hmac", string.Format("{0}:{1}:{2}:{3}", appId, requestSignatureBase64String, nonce, requestTimeStamp));
+            }
+
+            response = await base.SendAsync(request, cancellationToken);
+            return response;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,20 +2,30 @@
 
 This library is just for illustration. Be careful when you plan to use for production.
 
-Based on http://bitoftech.net/2014/12/15/secure-asp-net-web-api-using-api-key-authentication-hmac-authentication/ and https://github.com/johnhidey/Hmac .
+ASP.NET Core 2.0 port of https://github.com/ademcaglin/Security.HmacAuthentication/tree/master/tests/HmacTests which is based on http://bitoftech.net/2014/12/15/secure-asp-net-web-api-using-api-key-authentication-hmac-authentication/ and https://github.com/johnhidey/Hmac .
 
 # How to use
 
-Copy files in Security.HmacAuthentication/src/HmacAuthentication/ folder into your project and write following code in Startup.cs
+Copy files in Security.HmacAuthentication/HmacAuthentication/ folder into your project and write following code in Startup.cs
+		public void ConfigureServices(IServiceCollection services)
+        {
+            //...
+			services.AddAuthentication()
+					.AddHmacAuth(cfg =>
+					{
+						cfg.AppId = "<app-id>"
+						cfg.SecretKey = "<secret>";
+						cfg.MaxRequestAgeInSeconds = 500;
+					})
+            //...
+		}
 
         public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
             //...
-            app.UseHmacAuthentication(new HmacAuthenticationOptions
-            {
-                 SecretKey = "<secret>",
-                 AppId = "<app-id>",
-                 AutomaticAuthenticate =true
-           });
+            app.UseAuthentication();
            //...
          }
+
+To call a API, use a `HttpClient`with a `DelegatingHandler`. An example can be found in
+`Security.HmacAuthentication\HmacAuthentication\HmacDelegatingHandler.cs`
